30ffa84a35955a6408168b8fc877f99462be5f3c
Ade <mahyor.sam@gmail.com>
2024-05-05 22:21:28 +0300

    feat: add auth routes

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -1,10 +1,23 @@
 import { Router } from 'express';
-import { generateNonce } from '../controllers/auth';
+import {
+  generateNonce,
+  verify,
+  logout,
+  getActiveProfile,
+} from '../controllers/auth';
 
 const router = Router();
 
-router.post('/generate-nonce', async (request, response) =>
+router.get('/generate-nonce', async (request, response) =>
   generateNonce(request, response)
+);
+
+router.post('/verify', async (request, response) => verify(request, response));
+
+router.get('/logout', async (request, response) => logout(request, response));
+
+router.get('/profile', async (request, response) =>
+  getActiveProfile(request, response)
 );
 
 export default router;
